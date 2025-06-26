c91ca9056c0cbbeec30316ef86c03b38e619d426
Maaz-21 <155450768+Maaz-21@users.noreply.github.com>
2025-06-26 19:44:17 +0530

    Deploying frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
 let Is_prod= process.env.NODE_ENV === 'production';
-const server = Is_prod ? 'https://frame.onrender.com' : 'http://localhost:8000';
+const server = Is_prod ? 'https://frame-1ftf.onrender.com' : 'http://localhost:8000';
 
 export default server;
